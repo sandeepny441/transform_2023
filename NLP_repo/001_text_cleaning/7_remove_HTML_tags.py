86da81b5b2ac2b3b871020575213a70c9f296db4
Sandeep Reddy <sandeepny441@gmail.com>
2023-07-03 00:43:18 -0500

    added something

--- NLP_repo/001_text_cleaning/7_remove_HTML_tags.py
+++ NLP_repo/001_text_cleaning/7_remove_HTML_tags.py
@@ -1,1 +1,18 @@
+from bs4 import BeautifulSoup
+
+# Sample HTML text data
+html_text = '<html><body><p>This is an example <b>HTML</b> text with <a href="https://example.com">hyperlinks</a> and other <i>markup</i>.</p></body></html>'
+
+# Create a BeautifulSoup object
+soup = BeautifulSoup(html_text, 'html.parser')
+
+# Remove HTML tags and markup from the text
+processed_text = soup.get_text()
+
+# Print the processed text
+print(processed_text)
+
+
+print(300)
+
 
