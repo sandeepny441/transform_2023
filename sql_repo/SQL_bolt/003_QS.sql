296e6dbed74855bbaa6401b7c1702e6f405e5c7c
Sandeep Reddy <sandeepny441@gmail.com>
2023-07-06 21:21:13 -0500

    added sql bolt files

---
+++ sql_repo/SQL_bolt/003_QS.sql
@@ -0,0 +1,13 @@
+SELECT title, director FROM movies 
+WHERE title LIKE "Toy Story%";
+
+SELECT * FROM movies 
+WHERE director = 'John Lasseter'
+
+SELECT * FROM movies 
+WHERE director <> 'John Lasseter'
+
+SELECT * FROM movies 
+WHERE title like 'WALL-%'
+
+
