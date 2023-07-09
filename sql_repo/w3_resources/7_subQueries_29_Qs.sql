b2eb7fc589bbeed13660866ea30e5ed0fc22133a
Sandeep Reddy <sandeepny441@gmail.com>
2023-07-09 02:20:48 -0500

    added sql bolt files

--- sql_repo/w3_resources/7_subQueries_29_Qs.sql
+++ sql_repo/w3_resources/7_subQueries_29_Qs.sql
@@ -69,3 +69,76 @@ where customer.cust_id = orders.cust_id
 left outer join salesman
 where orders.salesman_id = salesman.salesman_id
 
+--12 
+select s.salesman_id from salesman 
+left outer join customer c
+on s.salesman_id = c.salesman_id 
+
+--13 
+SELECT a.cust_name,a.city,a.grade, 
+b.name AS "Salesman", 
+c.ord_no, c.ord_date, c.purch_amt 
+FROM customer a 
+RIGHT OUTER JOIN salesman b 
+ON b.salesman_id=a.salesman_id 
+RIGHT OUTER JOIN orders c 
+ON c.customer_id=a.customer_id;
+
+
+--14 Write a SQL statement to make a list for the salesmen who either work for one or more customers or yet to join any of the customer. The customer, may have placed, either one or more orders on or above order amount 2000 and must have a grade, or he may not have placed any order to the associated supplier.
+
+select * from customer c 
+full right join salesman s
+ON c.salesman_id = s.salesman_id 
+left join orders o
+where o.customer_id  = c.customer_id 
+
+--15 
+select * from customers 
+full left join orders 
+ON c.customer_id = o.customer_id
+
+--16
+SELECT a.cust_name,a.city, b.ord_no,
+b.ord_date,b.purch_amt AS "Order Amount" 
+FROM customer a 
+FULL OUTER JOIN orders b 
+ON a.customer_id=b.customer_id 
+WHERE a.grade IS NOT NULL;
+-- # DOUBT 
+
+--17 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
