35a4c500f1518a4fb14f9a8c21ed02597f6897ec
Sandeep Reddy <sandeepny441@gmail.com>
2023-07-12 05:48:58 -0500

    added sql files

--- sql_repo/SQL_theory_2/003_changes_to_tables/001_create.sql
+++ sql_repo/SQL_theory_2/003_changes_to_tables/001_create.sql
@@ -6,4 +6,21 @@ CREATE TABLE employees (
 )
 
 
+CREATE TABLE table_name (
+  column1 datatype1,
+  column2 datatype2,
+);
+
+CREATE TABLE Employees (
+  FirstName VARCHAR(50),
+  LastName VARCHAR(50),
+  Salary DECIMAL(10, 2)
+);
+
+CREATE TABLE Employees (
+  EmployeeID INT PRIMARY KEY,
+  FirstName VARCHAR(50) NOT NULL,
+  LastName VARCHAR(50) NOT NULL,
+  Salary DECIMAL(10, 2)
+);
 
---
+++ sql_repo/SQL_theory_2/006_window_functions/001_topics.sql
@@ -0,0 +1,11 @@
+PARTITION BY
+ORDER BY
+RANGE or ROW
+Ranking Window Functions
+RANK()
+DENSE_RANK()
+ROW_NUMBER()
+
+-- Aggregate 
+-- Ranking 
+-- Value 
