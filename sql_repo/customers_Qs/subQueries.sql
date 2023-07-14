fbe3193212aff65891627236a9374c23fea19020
Sandeep Reddy <sandeepny441@gmail.com>
2023-07-14 05:07:15 -0500

    added sql file

--- sql_repo/customers_Qs/subQueries.sql
+++ sql_repo/customers_Qs/subQueries.sql
@@ -32,3 +32,107 @@ where city = (
               order by avg_credit_limit 
               limit 1) as avg_SQ
               )
+
+-- Write a SQL subquery to find the city that has the highest number of customers with a credit limit over 50000.
+select city from customers 
+where city = (select city from 
+(select city, count(customernumber) as cust_count
+from customers 
+where creditlimit > 50000
+group by city
+order by cust_count desc
+limit 1 ) as sq_temp
+)
+
+
+-- Write a SQL subquery to find the salesRepEmployeeNumber who manages the customer with the highest creditLimit.
+SELECT salesRepEmployeeNumber 
+FROM customers 
+WHERE creditLimit = (
+  SELECT MAX(creditLimit) 
+  FROM customers
+)
+
+-- Write a SQL subquery to find all customers who live in the same state as the customer with the highest creditLimit.
+
+SELECT customernumber 
+FROM customers 
+WHERE state = (
+  SELECT state 
+  FROM customers 
+  WHERE creditLimit = (
+    SELECT MAX(creditLimit) 
+    FROM customers
+  )
+)
+
+-- Write a SQL subquery to find the country that has the most customers who have a creditLimit of at least 50000.
+
+select country from customers 
+where country = (select country from 
+(select country, count(custmernumber) as cust_count from customers 
+where creditlimit >= 50000
+group by country
+order by cust_count desc limit 1
+) as temp_SQ
+)
+
+-- Write a SQL subquery to find the salesRepEmployeeNumber that manages the most customers in the 'USA'
+
+select salesRepEmployeeNumber from customers 
+where salesRepEmployeeNumber = 
+select salesRepEmployeeNumber from
+(select salesRepEmployeeNumber, count(distinct customernumber) as cust_count
+from customers
+where coutry = 'USA'
+order by cust_count desc limit 1
+)
+
+-- Write a SQL subquery to find the city that has the most customers who have a creditLimit below 10000.
+select city from customers 
+where city = (select city from 
+(select city, count(customernumber) as cust_count from customers 
+where creditlimit < 10000
+order by cust_count desc 
+limit 1 
+) as temp_SQ 
+)
+
+-- Write a SQL subquery to find all customers who live in the same state as the customer with the lowest creditLimit.
+
+select customernumber from customers 
+where state = (select state from cusotmers 
+where creditlimit = (select min(creditlimit) from customers)
+)
+
+select customernumber from customers 
+where state = (select state from  customers 
+order by creditlimit 
+limit 1)
+
+-- Write a SQL subquery to find the salesRepEmployeeNumber that manages the customer with the lowest creditLimit
+
+select salesRepEmployeeNumber from customers
+where creditLimit = (select min(creditlimit) from customers)
+
+-- Write a SQL subquery to find the customernumber of the customer who resides in the city that has the most customers.
+
+select customernumber from customers
+where city = (select city from 
+(select city, count(customernumber) as cust_count
+from customers 
+group by city 
+order by cust_count desc limit 1) 
+as temp_SQ
+)
+
+-- Write a SQL subquery to find the country that has the fewest customers.
+
+select country from customers 
+where country = (select country from 
+(select country, count(distinct customernumber) as cust_count
+group by coutnry order by cust_count ASC
+limit 1) as temp_SQ
+)
+
+-- Write a SQL subquery to find the state that has the most customers with a creditLimit above 50000.
