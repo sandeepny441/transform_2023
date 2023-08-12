984e80729ba0ac9ffd8d0cb4de2114c40d2e3037
Sandeep Reddy <sandeepny441@gmail.com>
2023-08-12 14:27:34 -0500

    added topics

--- pandas_repo/python_files/001_series_methods.py
+++ pandas_repo/python_files/001_series_methods.py
@@ -7,9 +7,6 @@ s.nunique()
 s.isnull().sum()
 s.dropna()
 s.fillna(value)
-
-
-
 # sorting 
 s.sort_values()
 s.sort_index()
@@ -24,18 +21,41 @@ s.drop() # Drop values from rows (axis=0) or columns (axis=1)
 #membership 
 s.isin(values)
 
-
 s.drop_duplicates() # Drop duplicate values
 s.astype(dtype) # Cast values to dtype
 s.replace(val1, val2) # Replace values 
+
 s.rename() # Rename index labels
 s.between(val1, val2) # Filter between values
+s.clip(lower, upper) # Trim values to range
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 s.duplicated() # Check for duplicates
 s.interpolate() # Fill NaNs by interpolation 
-s.clip(lower, upper) # Trim values to range
-s.expanding(min_periods).mean() # Expanding statistics
 
+s.expanding(min_periods).mean() # Expanding statistics
 
 # apply functions 
 s.apply(func)
 s.map(dict)
+
